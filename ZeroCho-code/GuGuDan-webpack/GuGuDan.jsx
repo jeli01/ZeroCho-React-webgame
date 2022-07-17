144a03a2ec4005e0aafd7149441a3439fcb05e58
jeli01 <jeli01@naver.com>
2022-07-18 02:17:13 +0900

    2-6

---
+++ ZeroCho-code/GuGuDan-webpack/GuGuDan.jsx
@@ -0,0 +1,93 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class GuGuDan extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      first: Math.ceil(Math.random() * 9),
+      second: Math.ceil(Math.random() * 9),
+      value: '',
+      result: '',
+    }
+  }
+
+  onSubmit = (e)=> {
+    e.preventDefault();
+    if (parseInt(this.state.value) === this.state.first * this.state.second) {
+      this.setState((prevState) => {
+        return {
+          result: '정답:' + prevState.value,
+          first:Math.ceil(Math.random() * 9),
+          second: Math.ceil(Math.random() * 9),
+          value: '',
+        }
+      });
+      this.input.focus();
+    } else {
+      this.setState({
+        result: '땡',
+        value: '',
+      });
+    }
+  }
+
+  onChange = (e) => this.setState({value: e.target.value})
+
+  onRefInput = (c) => { this.input = c;};
+
+  input;
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>{this.state.first} 곱하기 {this.state.second}는?</div>
+        <form onSubmit={this.onSubmit}>
+          <input ref = {this.onRefInput}type="number" value={this.state.value} onChange={this.onChange}/>
+          <button>입력!</button>
+        </form>
+        <div>{this.state.result}</div>  
+      </React.Fragment>
+    );
+  }
+}
+
+function GuGuDan2() {
+  const [first, setFirst] = React.useState(Math.ceil(Math.random() * 9));
+  const [second, setSecond] = React.useState(Math.ceil(Math.random() * 9));
+  const [value, setValue] = React.useState('');
+  const [result, setResult] = React.useState('');
+  const inputRef = React.useRef(null);
+
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (parseInt(value) === first * second) {
+      setResult('정답: ' + value);
+      setFirst(Math.ceil(Math.random() * 9));
+      setSecond(Math.ceil(Math.random() * 9));
+      setValue('');
+      inputRef.current.focus();
+    } else {
+      setResult('떙');
+      setValue('');
+      inputRef.current.focus();
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <div>{first} 곱하기 {second}는? </div>
+      <form onSubmit={onSubmitForm}>
+        <input ref = {inputRef} onchange={onChangeInput} value={value}/>
+        <button>입력!</button>
+      </form>
+      <div id="result">{result}</div>
+    </React.Fragment>
+  )
+}
+
+module.exports = GuGuDan;
--- ZeroCho-code/webpack-tutorial/webpack.config.js
+++ ZeroCho-code/webpack-tutorial/webpack.config.js
@@ -5,12 +5,23 @@ module.exports = {
   mode: 'development', // 실서비스: production
   devtool: 'eval', // 빠르게 하겠다.
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'] 
   },
-
   entry: {
     app: ['./client'],
   }, // 입력
+  
+  module: {
+    rules: [{
+      test: /\.jsx?/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        plugins: ['@babel/plugin-proposal-class-properties'],
+      }
+    }],
+  },
+
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'app.js'
