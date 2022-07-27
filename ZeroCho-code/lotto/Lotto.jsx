ca6f1597893b1eea3648fb0ad2cb7c96294dd0e6
jeli01 <jeli01@naver.com>
2022-07-28 00:25:23 +0900

    6-4

--- ZeroCho-code/lotto/Lotto.jsx
+++ ZeroCho-code/lotto/Lotto.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useRef, useState } from 'react';
 import Ball from './Ball';
 
 function getWinNumbers() {
@@ -13,6 +13,51 @@ function getWinNumbers() {
   return [...winNumbers, bonusNumber];
 }  
 
+const Lotto2 = () => {
+  const [winNumbers, setWinNumbers] = useState(getWinNumbers());
+  const [winBalls, setWinBalls] = useState([]);
+  const [bonus, setBonus] = useState(null);
+  const [redo, setRedo] = useState(false);
+  const timeouts = useRef([]);
+
+  useEffect( () => {
+    for(let i = 0 ; i < winNumbers.length - 1; i++) {
+      timeouts.current[i] = setTimeout( () => {
+        setWinBalls((prevBalls) => [...prevBalls, winNumbers[i]]);
+      }, (i + 1) * 1000);
+    }
+    timeouts.current[6] = setTimeout( () => {
+      setBonus(winNumbers[6]);
+      setRedo(true);
+    }, 7000);
+    return () => {
+      timeouts.current.forEach((v) => {
+        clearTimeout(v);
+      });
+    }
+  }, [timeouts.current ]);  // 빈 배열이면 componentDidMount와 동일
+  // 배열에 요소가 있으면 componentDidMount랑 componentDidUpdate 둘 다 수행
+
+  const onClickRedo = () => {
+    setWinNumbers(getWinNumbers());
+    setWinBalls([]);
+    setBonus(null);
+    setRedo(false);
+    timeouts.current = [];
+  };
+
+  return (
+      <>
+        <div>당첨 숫자</div>
+        <div id="결과창">
+          {winBalls.map((v) => <Ball key={v} number={v} />)}
+        </div>
+        <div>보너스!</div>
+        {bonus && <Ball number={bonus} />}
+        {redo &&<button onClick={onClickRedo}> 한번 더!</button>}
+      </>
+  );
+}
 class Lotto extends Component {
   state = {
     winNumbers: getWinNumbers(), // 당첨 숫자들
@@ -83,4 +128,4 @@ class Lotto extends Component {
   }
 }
 
-export default Lotto;
+export default Lotto2;
