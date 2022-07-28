cd8a30f7a509fc1eb856fd1729d21b5e4f866e98
jeli01 <jeli01@naver.com>
2022-07-28 16:20:18 +0900

    7-1 && useReducer tutorial

---
+++ ZeroCho-code/tictacto/TicTacToe.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useReducer, useCallback } from 'react';
+import Table from './Table';
+
+const initialState = {
+  winner: '',
+  turn: 'O',
+  tableData: [['','',''],['','',''],['','','']],
+};
+
+const SET_WINNER = 'SET_WINNER';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    // state.winner = action.winner; 이렇게 하면 안됨.
+    case SET_WINNER:
+      return {
+        ...state,
+        winner: action.winner,
+      }
+  }
+};
+
+const TicTacToe = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // const [winner, setWinner] = useState('');
+  // const [turn, setTurn] = useState('O');
+  // const [tableData, setTableData] = useState([['','',''],['','',''],['','','']]);
+
+  const onClickTable = useCallback( () => {
+    dispatch({ type: SET_WINNER, winner: 'O' });
+  }, []);
+
+  return (
+    <>
+      <Table onClick={onClickTable} tableData={state.tableData} />
+      {state.winner && <div>{state.winner}님의 승리</div>}
+    </>
+  )
+};
+
+export default TicTacToe;
---
+++ ZeroCho-code/tictacto/client.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TicTacToe from './TicTacToe';
+// import UseReducer from './UseReducer2';
+
+ReactDOM.render(<TicTacToe />, document.querySelector('#root'));
+// ReactDOM.render(<UseReducer />, document.querySelector('#root'));
